830c2697df1ce10b953e1d00836b98b4f1f9d56a
giovanni-arduini <gionnibigudd@gmail.com>
2025-02-26 11:45:03 +0100

    ADD answer to question 3

--- code-questions.js
+++ code-questions.js
@@ -13,21 +13,45 @@
 
 //
 
-const hamburger = {
-  name: "Cheese Burger",
-  weight: 250,
-  ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
-};
+// const hamburger = {
+//   name: "Cheese Burger",
+//   weight: 250,
+//   ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+// };
 
-const secondBurger = { ...hamburger };
-secondBurger.ingredients[0] = "Salad";
+// const secondBurger = { ...hamburger };
+// secondBurger.ingredients[0] = "Salad";
 
-console.log(hamburger.ingredients[0]);
-("Salad");
-console.log(secondBurger.ingredients[0]);
-("Salad");
+// console.log(hamburger.ingredients[0]);
+// ("Salad");
+// console.log(secondBurger.ingredients[0]);
+// ("Salad");
 
 // sono stati creati due oggetti in memoria, il primo è burger
 // mentre il secondo è secondBurger, che è una shallow copy
 // del primo: ha copiato tutte le sue proprietà superficiali,
 // ma gli oggetti annidati sono passati per riferimento
+
+//
+
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  maker: {
+    name: "Anonymous Chef",
+    restaurant: {
+      name: "Hyur's Burgers",
+      address: "Main Street, 123",
+      isOpen: true,
+    },
+    age: 29,
+  },
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// saranno creati nove oggetti: il primo burger,
+// nel quale sono annidati il maker e il restaurant,
+// e poi due copie profonde di burger, che contengono
+// la stessa struttura del primo
